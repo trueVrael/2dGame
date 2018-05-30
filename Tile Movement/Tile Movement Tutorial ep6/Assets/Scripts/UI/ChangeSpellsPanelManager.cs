3e0d534af36ebcacd3571f47ce59177ae8d3a9c3
NolkMan <4n931mail@gmail.com>
2018-05-30 19:59:47 +0200

    Made a SpellChangePanel that will be controlled by SpellChangePanelManager.cs Made a ChangeSpellSingleSpellButton that is used to add avalialbe spells to SpellChangePanel Made a SpellChangeButton that when pressed will enable the SpellChangePanel

---
+++ Tile Movement/Tile Movement Tutorial ep6/Assets/Scripts/UI/ChangeSpellsPanelManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSpellsPanelManager : MonoBehaviour {
+    public GameObject SpellPanel;
+    public Button enableButton;
+    public GameObject changeSpellButtonPrefab;
+    private Player player;
+
+    private bool isActive = false;
+
+    private int CurrentI;
+    private List<int> spells;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+
+        enableButton.onClick.AddListener(EnableButtonPressed);
+
+        spells = new List<int>();
+
+        gameObject.SetActive(isActive);
+	}
+
+    // Update is called once per frame
+    private bool added = false;
+	void Update () {
+        if (!added) {
+            AddSpell(0); AddSpell(0);
+            added = true;
+        }
+	}
+
+    public void EnableButtonPressed() {
+        isActive = !isActive;
+        gameObject.SetActive(isActive);
+    }
+
+    public void AddSpell(int spellID) {
+        spells.Add(spellID);
+        GameObject newbutton = Instantiate(changeSpellButtonPrefab) as GameObject;
+        Image image = newbutton.transform.GetChild(0).GetComponent<Image>();
+        image.sprite = player.Spells[spellID].GetComponentInChildren<SpriteRenderer>().sprite;
+        image.preserveAspect = true;
+        newbutton.transform.SetParent(transform);
+
+        int newI = CurrentI;
+        CurrentI++;
+        newbutton.GetComponent<Button>().onClick.AddListener(() => ButtonPressed(newI));
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform) transform);
+    }
+
+    public void ButtonPressed(int buttonID) {
+        Debug.Log("Pressed button of ID: " + buttonID.ToString() + " that has spellID: " + spells[buttonID].ToString());
+    }
+}
