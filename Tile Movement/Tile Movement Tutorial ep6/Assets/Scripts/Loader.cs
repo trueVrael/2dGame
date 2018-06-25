83fda0cd21c53b42c698752f88983dd82db47438
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2018-06-25 14:31:44 +0200

    dźwięk wrogiej śmierci

--- Tile Movement/Tile Movement Tutorial ep6/Assets/Scripts/Loader.cs
+++ Tile Movement/Tile Movement Tutorial ep6/Assets/Scripts/Loader.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Loader : MonoBehaviour {
 
 	public GameObject gameManager;
+    public AudioSource enemyDeath;
 	// Use this for initialization
 	void Awake () {
 		if(GameManager.instance == null)
